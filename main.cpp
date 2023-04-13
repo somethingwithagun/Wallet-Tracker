c36bd9b9581348ad79717b02ba6b2b00111f175d
somethingwithagun <iamunknownsubject@gmail.com>
2023-04-13 07:34:44 +0300

    changed palette

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
+#include "palette.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    setPalette(a);
     MainWindow w;
     w.show();
     return a.exec();
---
+++ palette.h
@@ -0,0 +1,9 @@
+﻿#ifndef PALLETE_H
+#define PALLETE_H
+
+#include <QPalette>
+#include <QApplication>
+
+void setPalette(QApplication& app);
+
+#endif
